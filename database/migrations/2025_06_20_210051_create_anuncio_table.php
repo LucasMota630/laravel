8a9d40ee55e7959a4e337682408620b331fa68d3
LucasMota630 <motalucasmoraes@gmail.com>
2025-06-20 22:26:18 -0300

    tentativa de fazer relacionamento :(

--- database/migrations/2025_06_20_210051_create_anuncio_table.php
+++ database/migrations/2025_06_20_210051_create_anuncio_table.php
@@ -14,11 +14,28 @@ return new class extends Migration
         Schema::create('anuncio', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            // $table->unsignedBigInteger('id_veiculo');
             $table->string('titulo', 255);
             $table->string('descricao', 255);
             $table->string('preco', 255);
             $table->string('data_publicacao', 255);
+
+            // $table->foreign('id_veiculo')->references('id')->on('veiculo');
+            // $table->unique('id_veiculo');
+
+            // $table->foreign('id_proprietario')->references('id')->on('proprietario');
+
         });
+
+        // Schema::table('proprietario', function(Blueprint $table) {
+        //     $table->unsignedBigInteger('id_proprietario');
+        //     $table->foreign('id_proprietario')->references('id_proprietario')->on('anuncio');
+        // });
+
+        // Schema::table('veiculo', function(Blueprint $table) {
+        //     $table->unsignedBigInteger('id_veiculo');
+        //     $table->foreign('id_veiculo')->references('id_veiculo')->on('anuncio');
+        // });
     }
 
     /**
@@ -26,6 +43,15 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // Schema::table('proprietario', function(Blueprint $table) {
+        //     $table->dropForeign('proprietario_id_anuncio_foreign');
+        //     $table->dropColumn('id_anuncio');
+        // });
+
+        // Schema::table('anuncio', function(Blueprint $table) {
+        //     $table->dropForeign('anuncio_id_proprietario_foreign');
+        //     $table->dropColumn('id_proprietario');
+        // });
         Schema::dropIfExists('anuncio');
     }
 };
