9b7ae8a3588f3e7edfa6ba01c3670d7017dc7c42
LucasMota630 <motalucasmoraes@gmail.com>
2025-08-01 16:31:16 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/clientes', function(){
-    return [
-        ['nome' => 'NALDODOCANAL', 'email' => 'naldodocanal@hotmail.com'],
-        ['nome' => 'ZÉ BISCOITO', 'email' => 'ze.biscoito@pipoca.com'],
-        ['nome' => 'MARIA GASOSA', 'email' => 'maria.gasosa@refri.com'],
-        ['nome' => 'JULINHO DA VÁLVULA', 'email' => 'julinhodavalvula@pressao.net'],
-        ['nome' => 'TIÃO PIPOCA', 'email' => 'tiao.pipoca@cinema.org'],
-        ['nome' => 'BETO CATRACA', 'email' => 'beto.catraca@busmail.com'],
-        ['nome' => 'DONANA DA FEIRA', 'email' => 'donana.feira@horta.com'],
-        ['nome' => 'CHICO PIRULITO', 'email' => 'chico.pirulito@doces.net'],
-        ['nome' => 'LURDINHA DO TAXI', 'email' => 'lurdinha.taxi@corrida.com'],
-        ['nome' => 'RONALDO PÃO COM OVO', 'email' => 'ronaldo.paoovo@cafedamanha.org']
-    ];
-});
+Route::apiResource('/proprietario', 'App\Http\Controllers\ProprietarioController');
 
